e918685f503518b2542d1286d5178ebdd48e630a
Nicholas <nicholas@niceguys.com>
2013-12-28 20:41:41 -0800

    Finished extend, only accepts objects

--- src/underbar.js
+++ src/underbar.js
@@ -248,6 +248,14 @@ var _ = { };
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var args = Array.prototype.slice.call(arguments);
+    obj = args.shift();
+    _.each(args, function(innerobj) {
+      _.each(innerobj, function(value, key){
+        obj[key] = value;
+      });
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
